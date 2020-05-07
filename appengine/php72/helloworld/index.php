ba44a93ca56f000db0c81fe0effc5d9c07eb6532
yasho3115 <53729443+yasho3115@users.noreply.github.com>
2020-05-07 12:10:42 +1000

    Update index.php

--- appengine/php72/helloworld/index.php
+++ appengine/php72/helloworld/index.php
@@ -1,3 +1,16 @@
 <?php
 
 echo "hello world!";
+echo "StudentID:";
+echo " Name:";
+
+
+echo "Current Semester Enrolled Unit List";
+echo<table>
+echo “<tr><th>Unit Code</th><th>Unit Name</th><th>Credit</th></tr>”;
+
+echo "<tr><td>Hello world.</td><td>Hello world.</td><td>Hello world.</td><td>Hello world.</td></tr>";
+
+echo “</table”>;
+
+<?
